57cc821caf62ace66c5c5949219f2b7f45ae5338
PaulDiaz054 <pd1004405054@gmail.com>
2024-12-16 11:50:48 -0500

    consumo del web service tipo json

--- app/src/main/java/com/example/clienteconsumows/MainActivity.java
+++ app/src/main/java/com/example/clienteconsumows/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.clienteconsumows;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btnComunicar;
+    TextView tvRespuesta;
+    String respuesta = "";
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnComunicar = findViewById(R.id.btnComunicar);
+        tvRespuesta = findViewById(R.id.tvRespuesta);
+
+        btnComunicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConsumirWS();
+            }
+        });
+    }
+
+    public void ConsumirWS() {
+        String url = "http://10.10.12.254:3000/nombre";
+        OkHttpClient cliente = new OkHttpClient();
+        Request get = new Request.Builder()
+                .url(url)
+                .build();
+
+        cliente.newCall(get).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                MainActivity.this.runOnUiThread(() ->
+                        tvRespuesta.setText("Error al conectar con el servidor: " + e.toString())
+                );
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Unexpected code " + response);
+                    } else {
+                        String respuestaJson = responseBody.string();
+                        JSONArray lista = new JSONArray(respuestaJson);
+
+                        MainActivity.this.runOnUiThread(() -> {
+                            tvRespuesta.setText(lista.toString());
+                        });
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    MainActivity.this.runOnUiThread(() ->
+                            tvRespuesta.setText("Error al procesar la respuesta del servidor")
+                    );
+                }
+            }
+        });
     }
 }
